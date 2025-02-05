4da82463f4840042710e6b31b1d212e00407a84d
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2025-02-05 23:32:37 +0530

    update

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+        <nav>
+            <a href="">
+                <Image />
+            </a>
+        </nav>
+      
+    </>
+  )
+}
+
+export default Navbar
