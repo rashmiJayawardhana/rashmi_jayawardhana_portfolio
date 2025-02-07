6e01dd1139a155465a6f42e40e9c5072bd1297f4
Rashmi Jayawardhana <155569390+rashmiJayawardhana@users.noreply.github.com>
2025-02-08 00:31:22 +0530

    services

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -1,4 +1,4 @@
-import { serviceData } from '@/assets/assets'
+import { assets, serviceData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
@@ -9,11 +9,13 @@ const Services = () => {
         <h2 className='text-center text-5xl font-Ovo'>My Services</h2>
         <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>I am a fullstack developer.</p>
 
-        <div className='grid grid-cols-4 gap-6 my-10'>
+        <div className='grid grid-cols-auto gap-6 my-10'>
             {serviceData.map(({icon, title, description, link}, index)=>(
-                <div key={index}>
+                <div key={index} className='border border-gray-400 rounded-lg px-8 py-12 hover:shadow-black cursor-pointer hover:bg-lightHover hover:-translate-y-1 duration-500'>
                     <Image src={icon} alt='' className='w-10'/>
-                    <h3>{title}</h3>
+                    <h3 className='text-lg my-4 text-gray-700'>{title}</h3>
+                    <p className='text-sm text-gray-600 leading-5'>{description}</p>
+                    <a href={link} className='flex items-center gap-2 text-sm mt-5'>Read more <Image alt='' src={assets.right_arrow} className='w-4'/> </a>
 
                 </div>
             ))}
--- assets/assets.js
+++ assets/assets.js
@@ -103,7 +103,6 @@ export const serviceData = [
     { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
     { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
     { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
-    { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
 ]
 
 export const infoList = [
